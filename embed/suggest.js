8975eda2072ed9fa56ae75533f4d95a858ff4eb5
ChenYong <chenyong@jimeng.io>
2018-12-12 17:06:26 +0800

    add link to iOS

--- embed/suggest.js
+++ embed/suggest.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   var snippet =
-    '<div class="chrome-guide-container" style="background-color: rgb(232, 214, 125); color: rgb(128, 128, 128); font-size: 16px; left: 0px; line-height: 48px; position: fixed; text-align: center; top: 0px; width: 100%; z-index: 9999; font-family: &quot;Chinese Quote&quot;, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Microsoft YaHei&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;"><span>本网站建议使用谷歌浏览器打开, 推荐下载: </span><a href="https://pc.qq.com/detail/1/detail_2661.html" target="_blank">桌面版</a><span>, </span><a href="https://android.myapp.com/myapp/detail.htm?apkName=com.android.chrome" target="_blank">移动版</a><a class="chrome-guide-action" style="cursor: pointer; position: absolute; right: 8px; top: 0px;">关闭</a></div>';
+    '<div class="chrome-guide-container" style="background-color: rgb(232, 214, 125); color: rgb(128, 128, 128); font-size: 16px; left: 0px; line-height: 28px; position: fixed; text-align: center; top: 0px; width: 100%; z-index: 9999; font-family: &quot;Chinese Quote&quot;, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, &quot;PingFang SC&quot;, &quot;Hiragino Sans GB&quot;, &quot;Microsoft YaHei&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; padding: 16px 80px;"><span>本网站建议使用谷歌浏览器打开, 推荐下载: </span><a href="https://pc.qq.com/detail/1/detail_2661.html" target="_blank">PC版</a><span>，</span><a href="https://android.myapp.com/myapp/detail.htm?apkName=com.android.chrome" target="_blank">移动版</a><span>，</span><a href="https://itunes.apple.com/us/app/google-chrome/id535886823?mt=8" target="_blank">iOS版</a><span class="chrome-guide-action" style="color: rgb(24, 133, 242); cursor: pointer; position: absolute; right: 8px; top: 16px;">关闭</span></div>';
 
   var hookGuideAction = "chrome-guide-action";
 
@@ -26,8 +26,7 @@ window.addEventListener("load", function() {
     if (version < 40) {
       displaySuggestion();
     } else {
-      console.info("Chrome should be cool...");
-      displaySuggestion();
+      // console.info("Chrome is fine.");
     }
   }
 });
--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -19,7 +19,8 @@
            :top 0,
            :left 0,
            :width "100%",
-           :line-height "48px",
+           :line-height "28px",
+           :padding "16px 80px",
            :background-color (hsl 50 70 70),
            :text-align :center,
            :color (hsl 0 0 50),
@@ -28,19 +29,27 @@
            :font-family "Chinese Quote,-apple-system,BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif"}}
   (<> "本网站建议使用谷歌浏览器打开, 推荐下载: ")
   (a
-   {:inner-text "桌面版",
+   {:inner-text "PC版",
     :href "https://pc.qq.com/detail/1/detail_2661.html",
     :target "_blank"})
-  (<> ", ")
+  (<> "，")
   (a
    {:inner-text "移动版",
     :href "https://android.myapp.com/myapp/detail.htm?apkName=com.android.chrome",
     :target "_blank"})
-  (comment
-   a
+  (<> "，")
+  (a
+   {:inner-text "iOS版",
+    :href "https://itunes.apple.com/us/app/google-chrome/id535886823?mt=8",
+    :target "_blank"})
+  (span
    {:class-name "chrome-guide-action",
     :inner-text "关闭",
-    :style {:position :absolute, :right 8, :top 0, :cursor :pointer}})))
+    :style {:position :absolute,
+            :right 8,
+            :top 16,
+            :cursor :pointer,
+            :color "hsl(210, 89%, 52%)"}})))
 
 (defcomp
  comp-container
